db0535581fb98e155fcd01b9dc9d12cc74aef381
wm0x <aloh200200@gmail.com>
2025-03-29 09:50:39 +0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,24 @@
-"use client";
+"use client"
+import Benefits from "@/components/Benefits";
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import Hero from "@/components/Hero";
+import HowWeAre from "@/components/HowWeAre";
+import LanguageTicker from "@/components/language";
+import WhatIsSyntax from "@/components/WhatIsSyntax";
+
+
+
 export default function Home() {
   return (
     <div className="bg-gradient-to-br from-[#00FCA8] via-[#4C0DC8] to-[#4586FF] ">
-      {/* <Header /> */}
-      {/* <Hero /> */}
-      {/* <WhatIsSyntax /> */}
-      {/* <Benefits /> */}
-      {/* <HowWeAre /> */}
-      {/* <LanguageTicker /> */}
-      {/* <Footer /> */}
+      <Header />
+      <Hero />
+      <WhatIsSyntax />
+      <Benefits />
+      <HowWeAre />
+      <LanguageTicker />
+      <Footer />
     </div>
   );
 }
