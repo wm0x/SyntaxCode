974f15520d7377500894253df369e3b2512eb515
wm0x <aloh200200@gmail.com>
2025-03-29 09:57:48 +0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 // import Benefits from "@/components/Benefits";
 // import Footer from "@/components/Footer";
 import Header from "@/components/Header";
-// import Hero from "@/components/Hero";
+import Hero from "@/components/Hero";
 // import HowWeAre from "@/components/HowWeAre";
 // import LanguageTicker from "@/components/language";
 // import WhatIsSyntax from "@/components/WhatIsSyntax";
