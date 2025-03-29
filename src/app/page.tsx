14a6ea603490a6dcbc21a18ad9cee54de23edef0
wm0x <aloh200200@gmail.com>
2025-03-29 10:02:52 +0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,8 +13,8 @@ export default function Home() {
   return (
     <div className="bg-gradient-to-br from-[#00FCA8] via-[#4C0DC8] to-[#4586FF] ">
       <Header />
-      {/* <Hero />
-      <WhatIsSyntax />
+      <Hero />
+      {/* <WhatIsSyntax />
       <Benefits />
       <HowWeAre />
       <LanguageTicker />
