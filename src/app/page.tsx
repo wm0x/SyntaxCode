054103a1a7e7da55a43be2923e50d9bd2cf0486a
wm0x <aloh200200@gmail.com>
2025-03-29 09:56:40 +0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 "use client"
-import Benefits from "@/components/Benefits";
-import Footer from "@/components/Footer";
+// import Benefits from "@/components/Benefits";
+// import Footer from "@/components/Footer";
 import Header from "@/components/Header";
-import Hero from "@/components/Hero";
-import HowWeAre from "@/components/HowWeAre";
-import LanguageTicker from "@/components/language";
-import WhatIsSyntax from "@/components/WhatIsSyntax";
+// import Hero from "@/components/Hero";
+// import HowWeAre from "@/components/HowWeAre";
+// import LanguageTicker from "@/components/language";
+// import WhatIsSyntax from "@/components/WhatIsSyntax";
 
 
 
@@ -13,12 +13,12 @@ export default function Home() {
   return (
     <div className="bg-gradient-to-br from-[#00FCA8] via-[#4C0DC8] to-[#4586FF] ">
       <Header />
-      <Hero />
+      {/* <Hero />
       <WhatIsSyntax />
       <Benefits />
       <HowWeAre />
       <LanguageTicker />
-      <Footer />
+      <Footer /> */}
     </div>
   );
 }
